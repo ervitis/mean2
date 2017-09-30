90168f1605bea315d9689b4e6e35c20db14863a7
ervitis <vitomarti@gmail.com>
2017-10-01 00:04:30 +0200

    feat: more tests in album entity

--- test/models/test_model_album.js
+++ test/models/test_model_album.js
@@ -10,5 +10,17 @@ describe('testing album model schema', () => {
         AlbumSchema.prototype === mongoose.Schema.prototype;
 
         done();
+    });
+
+    it('should have these properties: title, description, year, image and artist', (done) => {
+        const Album = require('../../models/album');
+        const Artist = require('../../models/artist');
+        const artist = new Artist({name: 'dumb artist', description: 'dumb', image: 'photo1'});
+        const album = new Album({title: 'dumb album', description: 'album', year: 1997, image: 'photo', artist: artist});
+
+        expect(album.title).to.be.eql('dumb album');
+        expect(Object.keys(album).length).to.be.eql(4);
+
+        done();
     })
 });
