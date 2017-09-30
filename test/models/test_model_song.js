2cc20b2a399a757257438044d51978704aee8990
ervitis <vitomarti@gmail.com>
2017-09-30 19:26:00 +0200

    feat: more test in song model

--- test/models/test_model_song.js
+++ test/models/test_model_song.js
@@ -4,10 +4,25 @@ const mocha = require('mocha');
 const mongoose = require('mongoose');
 const expect = require('chai').expect;
 const SongSchema = require('../../models/song');
+const ObjectUtilities = require('../../utilities/objects');
 
 describe('testing song model schema', () => {
     it('should be a schema object', (done) => {
         SongSchema.prototype === mongoose.Schema.prototype;
+
+        done();
+    });
+
+    it('should have these properties: number, name, duration, file and album', (done) => {
+        const Song = require('../../models/song');
+        const Album = require('../../models/album');
+        const Artist = require('../../models/artist');
+        const artist = new Artist({name: 'dumb artist', description: 'dumb', photo: 'photo1'});
+        const album = new Album({title: 'dumb album', description: 'album', year: '1997', photo: 'photo', artist: artist});
+        const song = new Song({number: '1', name: 'dumb song name', duration: '3 min', file: 'path/file', album: album});
+
+        expect(song.name).to.be.eql('dumb song name');
+        expect(ObjectUtilities.size(song)).to.be.eql(4);
 
         done();
     })
