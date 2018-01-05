8db238ff728778c7cfd57931ebccd91930fc9a3d
ervitis <vitomarti@gmail.com>
2018-01-05 14:53:29 +0100

    feat: allow cors

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ const SongRoutes = require('./routes/song_route');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Acces-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
+
 app.use('/api', [UserRoutes, ArtistRoutes, AlbumRoutes, SongRoutes]);
 
 app.use((err, req, res, next) => {
